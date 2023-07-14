69ec708baf3eda6d68cbe2fe875b74bc4ecfb80e
Naim Miah <mmiah@fordham.edu>
2023-07-14 12:12:54 -0400

    addedbase node file

--- W3D5/practice-for-ch-ruby-poly-tree-node-long-practice/lib/tree_node.rb
+++ W3D5/practice-for-ch-ruby-poly-tree-node-long-practice/lib/tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
-  
+  attr_reader :children, :parent
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+
+
+
+
 end
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
