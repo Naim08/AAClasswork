0ac655b502f6bb6ecbc4abcda564e5a831a867c6
Naim Miah <mmiah@fordham.edu>
2023-07-18 09:28:41 -0400

    completed tic tac toe node project

--- W4D1/practice-for-ch-ruby-tic-tac-toe-ai-long-practice/lib/tic_tac_toe_node.rb
+++ W4D1/practice-for-ch-ruby-tic-tac-toe-ai-long-practice/lib/tic_tac_toe_node.rb
@@ -1,36 +1,59 @@
 require_relative "tic_tac_toe"
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark, :prev_move_pos, :children
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark # :x or :o
     @prev_move_pos = prev_move_pos # [x, y]
-    @children = []
   end
 
   def losing_node?(evaluator)
+    if @board.over?
+
+     return board.won? && board.winner != evaluator
+
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.all? {|child| child.losing_node?(evaluator)}
+    else
+      self.children.any? {|child| child.losing_node?(evaluator)}
+    end
   end
 
   def winning_node?(evaluator)
+    if @board.over?
+      return true if @board.winner == evaluator
+      return false if @board.winner == nil || @board.winner != evaluator
+    end
+
+    if @next_mover_mark == evaluator
+      children.any? {|child| child.winning_node?(evaluator)}
+    else
+      children.all? {|child| child.winning_node?(evaluator)}
+    end
+
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    child_arr = []
     mover_mark = @next_mover_mark == :x ? :o : :x
     (0..2).each do |row|
       (0..2).each do |col|
         pos = [row, col]
         if @board.empty?(pos)
           new_board = @board.dup
-          new_board[pos] = mover_mark
-          @children << TicTacToeNode.new(new_board, mover_mark, pos)
+          new_board[pos] = @next_mover_mark
+          child_arr << TicTacToeNode.new(new_board, mover_mark, pos)
         end
       end
     end
-    @children
+    child_arr
   end
+
 end
 
 # board = Board.new
